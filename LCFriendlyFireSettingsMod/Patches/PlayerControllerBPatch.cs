5d2d48b38161944b76714eaacb9e61a3b9d9e19b
snowl <snowl@DESKTOP-0G31MLE>
2023-12-29 00:44:21 -0600

    Add project files.

---
+++ LCFriendlyFireSettingsMod/Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,28 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace LCFriendlyFireSettingsMod.Patches
+{
+    [HarmonyPatch]
+    internal class PlayerControllerBPatch
+    {
+        [HarmonyPatch(typeof(PlayerControllerB), "DamagePlayerFromOtherClientServerRpc")]
+        [HarmonyPrefix]
+        public static void DamagePlayerFromOtherClientServerRpc(PlayerControllerB __instance, ref int damageAmount, Vector3 hitDirection, int playerWhoHit)
+        {
+            if (__instance.isInsideFactory != true)
+            {
+                LCFriendlyFireSettingsModBase.LoggerInstance.LogDebug($"Player{playerWhoHit} attempted to friendly fire, setting damage amount to 0.");
+                damageAmount = 0;
+            }
+        }
+    }
+}
---
+++ LCFriendlyFireSettingsMod/Plugin.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using System;
+using System.Collections.Generic;
+using BepInEx.Logging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using GameNetcodeStuff;
+
+namespace LCFriendlyFireSettingsMod.Patches
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class LCFriendlyFireSettingsModBase : BaseUnityPlugin
+    {
+        private const string modGUID = "Posiedon.LCTutorialMod";
+        private const string modName = "LC Tutorial Mod";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+        private static LCFriendlyFireSettingsModBase Instance;
+        public static ManualLogSource LoggerInstance { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            LoggerInstance = this.Logger;
+            LoggerInstance.LogInfo($"Plugin {modName} loaded successfully.");
+            harmony.PatchAll(typeof(LCFriendlyFireSettingsModBase));
+            harmony.PatchAll(typeof(PlayerControllerBPatch));
+        }
+    }
+}
